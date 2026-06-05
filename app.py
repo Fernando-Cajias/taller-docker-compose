00ce70f3115bb4bc4b18bfd3df601b7767ad4bef
Fernando Cajias <fdv.cajias@yavirac.edu.ec>
2026-06-04 20:43:34 -0500

    Fix: Corrección de indentación en app.py para producción

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
- import os
+import os
 import time
 from flask import Flask
 import psycopg2
@@ -6,7 +6,6 @@ import psycopg2
 app = Flask(__name__)
 
 def get_db_connection():
-    # Intenta conectarse usando las variables de entorno que definiremos en el compose
     return psycopg2.connect(
         host=os.environ.get('DB_HOST', 'db'),
         database=os.environ.get('DB_NAME', 'taller_db'),
@@ -23,9 +22,9 @@ def index():
         db_version = cur.fetchone()
         cur.close()
         conn.close()
-        return f'<h1>🚀 Taller 3.0: Flask + PostgreSQL Exitoso</h1><p>Conectado a: {db_version[0]}</p><p>Autor: Fernando Cajias</p>'
+        return f'<h1> Taller 3.0: Flask Exitoso</h1><p>Conectado a: {db_version[0]}</p><p>Autor: Fernando Cajias</p>'
     except Exception as e:
-        return f'<h1>❌ Error de conexión a la Base de Datos</h1><p>{str(e)}</p>'
+        return f'<h1> Modo Local (Sin Base de Datos)</h1><p>La aplicacion Flask funciona correctamente en la maquina.</p><p>Error de DB esperado: {str(e)}</p>'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
